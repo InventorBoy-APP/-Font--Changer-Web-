3c041138eaa4e2d2683cd33e5df42328a7d3fdfd
InventorBoy-APP <78768396+InventorBoy-APP@users.noreply.github.com>
2021-11-29 19:41:03 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 function setup(){
     canvas = createCanvas(600,500);
     canvas.position(840,150)
+    video = createCapture(VIDEO);
+    video.size(600,500);
+    video.position(140,150)
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+    }
+
+
+    function modelLoaded(){
+        console.log(" PoseNet Has Been Initialized ");
     }
     
     
@@ -12,3 +22,10 @@ function setup(){
     function preload(){
     " "
     }
+
+
+    function gotPoses(results){
+        if(results.length > 0){
+            console.log(results);
+        }
+      }
